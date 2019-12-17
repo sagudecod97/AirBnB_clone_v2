1c6cd486b3d6c86e89a7b191bf156cd49e017211
Santiago <sagudecod97@gmail.com>
2019-12-17 23:14:52 +0000

    Created method all, save, new and delete

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,7 +10,7 @@ class DBStorage:
     """ Class to manage the storage in th DataBase
         Args:
         __engine(str): The engine
-        __session(str): The session    
+        __session(str): The session
     """
     __engine = None
     __session = None
@@ -25,9 +25,45 @@ class DBStorage:
             format(user, pwd, hst, db), pool_pre_ping=True)
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine)
-        session = Session()
+        self.__session = Session()
         hbnb_env = environ.get('HBNB_ENV')
         if hbnb_env == 'test':
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Prints all the objects """
+
+        classes = ["User", "State", "City", "Amenity", "Place", "Review"]
+        dict_return = {}
+
+        if cls is None:
+            for table_name in classes:
+                for table in self.__session.query(table_name).all():
+                    dict_return["{}.{}".format(table_name, table.id)] = table
+        else:
+            for table in self.__session.query(cls).all():
+                dict_return["{}.{}".format(cls, table.id)] = table
+
+        #for key,value in dict_return.items():
+         #   print("******* {} *******".format(dict_return[key]))
+
+        return dict_return
+
+    def new(self, obj):
+        """ add the object to the current database session """
+        self.__session.add(obj)
+
+    def save(self):
+        """  commit all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from the current database session """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ create all tables in the database """
+
 
 
