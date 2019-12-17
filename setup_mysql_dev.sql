65413bafe0110985473e301e4e87b8acc28685ae
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-17 13:42:32 -0500

    Add feature : SQL configuration

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Sets up the SQL server
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
