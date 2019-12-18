cf9dbc795e5fd74c53b697301a9ddc2c6365dbb4
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-18 15:13:42 -0500

    Add Feature : Add relationship cascade

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -13,4 +14,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-
+    state = relationship("State", back_populates='cities',
+        cascade="all, delete, delete-orphan")
