f083aee4f501f4d837fdbd43b1914a9688140537
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-19 17:28:38 +0000

    Fix Feature : Comment FS

--- models/city.py
+++ models/city.py
@@ -16,13 +16,13 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
     glb_storage = environ.get('HBNB_TYPE_STORAGE')
-    if glb_storage != 'db':
+    """if glb_storage != 'db':
         @property
         def cities(self):
-            """Return all cities related with the state"""
+            Return all cities related with the state
             all_cities = models.storage.all("City")
             own_city = []
             for value in all_cities.values():
                 if self.id == value.state_id:
                     own_city.append(value)
-            return own_city
+            return own_city"""
