69a340756168be669b8b31165647f9784c14eb97
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-19 17:15:32 +0000

    Fix Feature : Fix relation MtoO

--- models/state.py
+++ models/state.py
@@ -14,4 +14,4 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all, delete")
+    cities = relationship('City', backref='state_all', cascade="delete")
