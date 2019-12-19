1d771d5b474b21e87f3cbb60a977a1b951b81df8
Santiago <sagudecod97@gmail.com>
2019-12-19 21:42:12 +0000

    Fixed ForeignKey error

--- models/place.py
+++ models/place.py
@@ -31,5 +31,5 @@ class Place(BaseModel, Base):
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
