0a07bcfa77ea0ec465ec15853dee97e56a241fef
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-20 18:18:59 +0000

    Fix indentation error

--- models/base_model.py
+++ models/base_model.py
@@ -34,8 +34,8 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
