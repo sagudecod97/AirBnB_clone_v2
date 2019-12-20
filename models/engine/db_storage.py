413572e23157e342266f2149db49257670b70e33
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-20 14:50:03 +0000

    Fix feature: Fixing import way Review

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,6 +7,7 @@ from models.city import City
 from models.state import State
 from models.user import User
 from models.place import Place
+from models.review import Review
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import scoped_session
 
