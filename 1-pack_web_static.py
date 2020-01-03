9637dd06c7c9aa47ed3c195793082d117704f8cf
Santiago <sagudecod97@gmail.com>
2020-01-03 22:09:57 +0000

    Created file 1-pack_web...

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from fabric.api import local
+from datetime import datetime
+"""
+Creates a .tgz archive from the web_static folder's archives
+"""
+
+
+def do_pack():
+
+    date = [
+        datetime.now().year,
+        datetime.now().month,
+        datetime.now().day,
+        datetime.now().hour,
+        datetime.now().minute,
+        datetime.now().second()
+    ]
+    directory = "web_static"
+    file_tgz = "{}_{}{}{}{}{}{}.tgz".format(directory,
+                                            date[0], date[1],
+                                            date[2], date[3],
+                                            date[4], date[4],
+                                            date[5])
+
+    local("mkdir -p versions")
+    local("tar cavf versions/{} {}".format(file_tgz, directory))
