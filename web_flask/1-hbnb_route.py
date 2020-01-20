4ca90ff7d7e9a1a242de3380cac32a3534a04c23
Dioni1195 <dionisio.arango95@gmail.com>
2020-01-20 19:22:43 +0000

    Adding a new route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from flask import Flask
+""" This is a Hello world code with different routes """
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Prints Hello HBNB """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints HBNB """
+    return 'HBNB'
+
+app.run()
