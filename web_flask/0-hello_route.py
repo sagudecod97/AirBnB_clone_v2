a9ba7bf5ba72959e2117b8ed472d65a414f0a5a4
sagudecod97 <sagudecod97@gmail.com>
2020-01-21 16:18:40 +0000

    Modified host and port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,4 +10,7 @@ app = Flask(__name__)
 def hello():
     """ Prints Hello HBNB """
     return 'Hello HBNB!'
-app.run(host='0.0.0.0')
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
