438878e797864468d8deff136c248d8d9535371d
sagudecod97 <sagudecod97@gmail.com>
2020-01-21 19:41:10 +0000

    Receives a number and prints it into template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def number(n):
 @app.route('/number_template/<int:number>', strict_slashes=False)
 def number_template(number):
     """ Render a template"""
-    return render_template('5-number.html', number=number)
+    return render_template('5-number.html', n=number)
 
 
 if __name__ == "__main__":
